d79083241a8b0f33288cb1d8823cd3b5aa114e1e
kexuefanqiangaixuexi <126554453+kexuefanqiangaixuexi@users.noreply.github.com>
2026-05-23 23:01:07 +0800

    feat: export validate, bump to 0.2.0

--- json_toolkit/__init__.py
+++ json_toolkit/__init__.py
@@ -1,6 +1,7 @@
 from .pretty import pretty_print
 from .diff import diff_files
 from .query import query_path
+from .validate import validate
 
-__version__ = "0.1.0"
-__all__ = ["pretty_print", "diff_files", "query_path"]
+__version__ = "0.2.0"
+__all__ = ["pretty_print", "diff_files", "query_path", "validate"]
